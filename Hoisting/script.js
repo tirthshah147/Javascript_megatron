20ee9564b3b37d6b3178b64ce1e068627bc95140
Tirth Shah <themadpreneur@Tirths-MacBook-Air.local>
2021-11-19 22:07:38 +0530

    Hoisting & template literals

---
+++ Hoisting/script.js
@@ -0,0 +1,115 @@
+
+// console.log(x);
+// var x = "Tirth";
+// console.log(x);
+
+
+
+
+
+
+
+
+// var x = 2;
+// function run(){
+//     console.log(x);
+//     var x = 3;
+// }
+
+// run();
+
+// var x = 3;
+// console.log(x); //undefined
+// x = 3;
+// //functional scope
+
+
+// var x = 2;
+// function run(){
+//     console.log(x);
+//     var x = 3;
+//     console.log(x);
+// }
+// run();
+// console.log(x);
+// // global scope
+
+
+// var x = 2;
+// function run(){
+//     var x = 3;
+//     console.log(x);
+// }
+
+// function fun(){
+//     console.log(x);
+// }
+
+// fun(); //2
+// run(); //3
+
+
+//Final questions
+// var x = 2;
+// console.log(x);     
+// function run1(){
+//     console.log(x);
+//     var x = 3;
+// }
+
+// var x = 4;
+// function run2(){
+//     console.log(x);
+// }
+// console.log(x);
+
+// run1();
+// run2();
+
+
+
+// run();
+// function run(){
+//     console.log("tirth");
+// }
+
+// x;
+// var x = 10;
+
+//function expression
+
+// console.log(run);
+// var run = function(){
+//     console.log('Tirth')
+// }
+
+// var run = () => {
+//     console.log('tirth');
+// }
+
+
+//temporal dead zone
+
+// let b = 3;
+// console.log(b); //6
+
+// // Gumnam Space
+// b = 3;
+
+
+// function run(){
+//     cobsole.log("Run")
+//  }
+ 
+// console.log("Tirth");
+
+
+// run();
+
+
+
+
+
+
+
+
---
+++ Template Literals/script.js
@@ -0,0 +1,4 @@
+var name = "Badri";
+var age  = 32;
+console.log(`My name' is ${name} & my age is ${age + 2}`);
+console.log('My names\' is ' + name + '& my age is ' + (age+2));
