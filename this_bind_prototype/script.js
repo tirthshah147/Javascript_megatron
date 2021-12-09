1a78a5a42d82d19ff9a4f6862f42eff2424a4afd
Tirth Shah <themadpreneur@Tirths-MacBook-Air.local>
2021-12-09 22:13:55 +0530

    this & prototype

---
+++ this_bind_prototype/script.js
@@ -0,0 +1,87 @@
+
+
+// console.log(this);
+
+// function run(){
+//     console.log(this);
+// }
+
+// run();
+
+
+// function Run(){
+//     console.log(this);
+// }
+
+// new empty object gets created  {}
+// this will be pointed to this empty obj
+// constructopr function runs!
+
+// var obj = new Run();
+
+
+
+// class Run{
+//     constructor(){
+//         this.eat = "i am eating!";
+//     }
+// }
+
+//new empty object gets created {}
+//this will be pointed to this empty obj
+//constructopr function runs!
+
+// var obj1 = new Run();
+
+// function run(){
+//     console.log(this);
+// }
+
+// var obj = {name:"Tirth"};
+
+// return function(){
+//     console.log(this); //obj
+// }
+
+// var bindedRun = run.bind(obj);
+
+// bindedRun();
+
+
+
+// Array.prototype
+
+// var arr = [1,2,3,4]
+// arr.__proto__ = Array.prototype
+// arr.__proto__ = {name:"tirth"}
+
+// function run(){
+//     console.log("i am running");
+// }s
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
